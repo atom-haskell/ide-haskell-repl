17f235d9156ec3faa1661e407b5e66ba9d2e8ed5
Nikolay Yakimov <root@livid.pp.ru>
2017-03-16 19:48:02 +0300

    Split view into interface and functionality somewhat

---
+++ src/ide-haskell-repl-base.ts
@@ -0,0 +1,291 @@
+import {
+  CompositeDisposable,
+  TextEditor,
+} from 'atom'
+import * as Util from 'atom-haskell-utils'
+import highlightSync = require('atom-highlight')
+import {filter} from 'fuzzaldrin'
+
+import {Button} from './button'
+import {CommandHistory} from './command-history'
+import {Editor} from './editor'
+import {GHCI} from './ghci'
+
+type UPI = any
+
+export interface IViewState {
+  uri?: string
+  history?: string[]
+  autoReloadRepeat?: boolean
+  content?: IContentItem[]
+}
+
+export interface IContentItem {
+  text: string
+  cls: string
+  hl?: boolean
+}
+
+type Severity = 'error' | 'warning' | 'repl' | string
+
+export interface IErrorItem {
+  uri?: string,
+  position?: [number, number],
+  message: string,
+  context?: string,
+  severity: Severity,
+  _time: number,
+}
+
+declare interface IMyString extends String {
+  trimRight (): IMyString
+}
+
+export abstract class IdeHaskellReplBase {
+  protected ghci: GHCI
+  protected cwd: AtomTypes.Directory
+  protected prompt: string
+  protected upi: UPI
+  protected messages: IContentItem[]
+  protected errors: IErrorItem[]
+  protected _autoReloadRepeat: boolean
+  protected history: CommandHistory
+  protected uri: string
+  constructor (upiPromise, {
+    uri, content, history, autoReloadRepeat = atom.config.get('ide-haskell-repl.autoReloadRepeat'),
+  }: IViewState) {
+    this.uri = uri
+    this.history = new CommandHistory(history)
+    this._autoReloadRepeat = autoReloadRepeat
+    this.errors = []
+
+    this.messages = content || []
+
+    setImmediate(async () => this.initialize(upiPromise))
+  }
+
+  public abstract update ()
+
+  public toggleAutoReloadRepeat () {
+    this.autoReloadRepeat = ! this.autoReloadRepeat
+  }
+
+  public async runCommand (command: string) {
+    let inp = command.split('\n')
+    let res = await this.ghci.writeLines(inp, (type, text) => {
+      console.error(type, text)
+      switch (type) {
+        case 'stdin':
+          text && this.messages.push({text: inp.join('\n'), hl: true, cls: 'ide-haskell-repl-input-text'})
+          break
+        case 'stdout':
+          text && this.messages.push({text, hl: true, cls: 'ide-haskell-repl-output-text'})
+          break
+        case 'prompt':
+          this.prompt = text[1]
+          break
+        default: break
+      }
+      this.update()
+    })
+    this.errorsFromStderr(res.stderr)
+    return res
+  }
+
+  public async ghciReload () {
+    return this.ghci.reload()
+  }
+
+  public async ghciReloadRepeat () {
+    let {stderr} = await this.ghci.reload()
+    if (! this.errorsFromStderr(stderr)) {
+      let command = this.history.goBack('')
+      return this.runCommand(command)
+    }
+  }
+
+  public set autoReloadRepeat (autoReloadRepeat: boolean) {
+    this._autoReloadRepeat = autoReloadRepeat
+    this.update()
+  }
+
+  public get autoReloadRepeat () {
+    return this._autoReloadRepeat
+  }
+
+  public interrupt () {
+    this.ghci.interrupt()
+  }
+
+  public async getCompletions (prefix) {
+    if (!prefix.trim()) {
+      return []
+    }
+    let {stdout} = await this.ghci.sendCompletionRequest()
+    stdout.shift()
+    return filter(stdout, prefix).map((text) => ({text: text.slice(1, -1)}))
+  }
+
+  protected async destroy () {
+    if (this.ghci) {
+      this.ghci.destroy()
+    }
+  }
+
+  protected async initialize (upiPromise: Promise<UPI>) {
+    this.upi = await upiPromise
+    if (!this.upi) { return this.runREPL(null) }
+
+    try {
+      let builder = await this.upi.params.get('ide-haskell-cabal', 'builder')
+      this.runREPL((builder || {}).name)
+    } catch (error) {
+      if (error) {
+        atom.notifications.addFatalError(error.toString(), {
+          detail: error,
+          dismissable: true,
+        })
+        this.destroy()
+      } else {
+        atom.notifications.addWarning("Can't run REPL without knowing what builder to use")
+        this.destroy()
+      }
+    }
+  }
+
+  protected async runREPL (builder: string) {
+    if (!builder) { builder = atom.config.get('ide-haskell-repl.defaultRepl') }
+    let subst = {
+      'nix-build': 'cabal',
+      'none': 'ghci',
+    }
+    builder = (subst[builder] || builder)
+
+    this.cwd = Util.getRootDir(this.uri)
+
+    let [cabalFile] =
+      this.cwd.getEntriesSync().filter((file) =>
+        file.isFile() && file.getBaseName().endsWith('.cabal'))
+
+    let cabal, comp
+    if (cabalFile) {
+      let cabalContents = cabalFile.readSync()
+      cabal = Util.parseDotCabalSync(cabalContents)
+      [comp] = Util.getComponentFromFileSync(cabalContents, this.cwd.relativize(this.uri))
+    }
+    let commandPath = atom.config.get(`ide-haskell-repl.${builder}Path`)
+
+    let args = {
+      stack: ['ghci'],
+      cabal: ['repl'],
+      ghci: [],
+    }
+    let extraArgs = {
+      stack: (x) => '--ghci-options="#{x}"',
+      cabal: (x) => '--ghc-option=#{x}',
+      ghci: (x) => x,
+    }
+
+    if (!args[builder]) { throw new Error('Unknown builder #{builder}') }
+    let commandArgs = args[builder]
+
+    commandArgs.push(...(atom.config.get('ide-haskell-repl.extraArgs').map(extraArgs[builder])))
+
+    if (comp) {
+      if (builder === 'stack') {
+        if (comp.startsWith('lib:')) {
+          comp = 'lib'
+        }
+        comp = `${cabal.name}:${comp}`
+        commandArgs.push('--main-is', comp)
+      } else { commandArgs.push(comp) }
+    }
+
+    this.ghci = new GHCI({
+      atomPath: process.execPath,
+      command: commandPath,
+      args: commandArgs,
+      cwd: this.cwd.getPath(),
+      onExit: async (code) => this.destroy(),
+    })
+
+    await this.ghci.waitReady()
+
+    this.ghci.load(this.uri, (type, text) => {
+      if (type === 'prompt') {
+        this.prompt = text[1]
+        this.update()
+      }
+    })
+  }
+
+  protected errorsFromStderr (stderr: string[]): boolean {
+    this.errors = this.errors.filter(({_time}) => Date.now() - _time < 10000)
+    let hasErrors = false
+    for (let err of stderr.join('\n').split(/\n(?=\S)/)) {
+      if (err) {
+        let error = this.parseMessage(err)
+        this.errors.push(error)
+        if (error.severity === 'error') {
+          hasErrors = true
+        }
+      }
+    }
+    if (this.upi) {
+      this.upi.messages.set(this.errors)
+    } else {
+      this.update()
+    }
+    return hasErrors
+  }
+
+  protected unindentMessage (message): string {
+    let lines = message.split('\n').filter((x) => !x.match(/^\s*$/))
+    let minIndent = null
+    for (let line of lines) {
+      let match = line.match(/^\s*/)
+      let lineIndent = match[0].length
+      if (lineIndent < minIndent || !minIndent) { minIndent = lineIndent }
+    }
+    console.error(minIndent, lines)
+    if (minIndent) {
+      lines = lines.map((line) => line.slice(minIndent))
+    }
+    return lines.join('\n')
+  }
+
+  protected parseMessage (raw): IErrorItem {
+    let matchLoc = /^(.+):(\d+):(\d+):(?: (\w+):)?\s*(\[[^\]]+\])?/
+    if (raw && raw.trim() !== '') {
+      let matched = raw.match(matchLoc)
+      if (matched) {
+        let msg = raw.split('\n').slice(1).join('\n')
+        let [file, line, col, rawTyp, context]: String[] = matched.slice(1)
+        let typ: Severity = rawTyp ? rawTyp.toLowerCase() : 'error'
+        if (file === '<interactive>') {
+          file = null
+          typ = 'repl'
+        }
+
+        // NOTE: this is done because typescript insists strings dont have
+        // trimRight() method
+        let msgany = msg as IMyString
+
+        return {
+          uri: file ? this.cwd.getFile(this.cwd.relativize(file)).getPath() : null,
+          position: [parseInt(line as string, 10) - 1, parseInt(col as string, 10) - 1],
+          message: this.unindentMessage(msgany.trimRight()),
+          context: context as string,
+          severity: typ,
+          _time: Date.now(),
+        }
+      } else {
+        return {
+          message: raw,
+          severity: 'repl',
+          _time: Date.now(),
+        }
+      }
+    }
+  }
+}
--- src/ide-haskell-repl-view.tsx
+++ src/ide-haskell-repl-view.tsx
@@ -11,66 +11,29 @@ import {Button} from './button'
 import {CommandHistory} from './command-history'
 import {Editor} from './editor'
 import {GHCI} from './ghci'
+import {
+  IContentItem,
+  IdeHaskellReplBase,
+  IViewState,
+} from './ide-haskell-repl-base'
+
+export {IViewState, IContentItem}
 
 const termEscapeRx = /\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]/g
-
-type UPI = any
-
-export interface IViewState {
-  uri?: string
-  history?: string[]
-  autoReloadRepeat?: boolean
-  content?: IContentItem[]
-}
 
 interface IViewStateOutput extends IViewState {
   deserializer: string
 }
 
-export interface IContentItem {
-  text: string
-  cls: string
-  hl?: boolean
-}
-
-type Severity = 'error' | 'warning' | 'repl' | string
-
-export interface IErrorItem {
-  uri?: string,
-  position?: [number, number],
-  message: string,
-  context?: string,
-  severity: Severity,
-  _time: number,
-}
-
-declare interface IMyString extends String {
-  trimRight (): IMyString
-}
-
-export class IdeHaskellReplView {
+export class IdeHaskellReplView extends IdeHaskellReplBase {
   public refs: {[key: string]: any}
   public editor: TextEditor
-  private ghci: GHCI
-  private cwd: AtomTypes.Directory
-  private prompt: string
-  private upi: UPI
   private outputFontFamily: any
   private outputFontSize: any
-  private messages: IContentItem[]
-  private errors: IErrorItem[]
-  private autoReloadRepeat: boolean
-  private history: CommandHistory
-  private uri: string
   private disposables: CompositeDisposable
-  constructor (upiPromise, {
-    uri, content, history, autoReloadRepeat = atom.config.get('ide-haskell-repl.autoReloadRepeat'),
-  }: IViewState) {
-    this.uri = uri
-    this.history = new CommandHistory(history)
-    this.setAutoReloadRepeat(autoReloadRepeat)
+  constructor (upiPromise, state: IViewState) {
+    super(upiPromise, state)
     this.disposables = new CompositeDisposable()
-    this.errors = []
 
     this.editor = new TextEditor({
       lineNumberGutterVisible: false,
@@ -79,8 +42,6 @@ export class IdeHaskellReplView {
     })
 
     atom.textEditors.add(this.editor)
-
-    this.messages = content || []
 
     this.disposables.add(
       atom.workspace.observeTextEditors((editor) => {
@@ -98,11 +59,7 @@ export class IdeHaskellReplView {
       this.outputFontFamily = fontFamily
     }))
 
-    this.cwd = Util.getRootDir(this.uri)
-
     etch.initialize(this)
-
-    this.initialize(upiPromise)
   }
 
   public async execCommand () {
@@ -117,28 +74,6 @@ export class IdeHaskellReplView {
     this.editor.element.focus()
   }
 
-  public async runCommand (command: string) {
-    let inp = command.split('\n')
-    let res = await this.ghci.writeLines(inp, (type, text) => {
-      console.error(type, text)
-      switch (type) {
-        case 'stdin':
-          text && this.messages.push({text: inp.join('\n'), hl: true, cls: 'ide-haskell-repl-input-text'})
-          break
-        case 'stdout':
-          text && this.messages.push({text, hl: true, cls: 'ide-haskell-repl-output-text'})
-          break
-        case 'prompt':
-          this.prompt = text[1]
-          break
-        default: break
-      }
-      this.update()
-    })
-    this.errorsFromStderr(res.stderr)
-    return res
-  }
-
   public historyBack () {
     let current = this.editor.getText()
     this.editor.setText(this.history.goBack(current))
@@ -146,44 +81,6 @@ export class IdeHaskellReplView {
 
   public historyForward () {
     this.editor.setText(this.history.goForward())
-  }
-
-  public async ghciReload () {
-    return this.ghci.reload()
-  }
-
-  public async ghciReloadRepeat () {
-    let {stderr} = await this.ghci.reload()
-    if (! this.errorsFromStderr(stderr)) {
-      let command = this.history.goBack('')
-      return this.runCommand(command)
-    }
-  }
-
-  public toggleAutoReloadRepeat () {
-    this.setAutoReloadRepeat(!this.getAutoReloadRepeat())
-  }
-
-  public setAutoReloadRepeat (autoReloadRepeat: boolean) {
-    this.autoReloadRepeat = autoReloadRepeat
-    this.update()
-  }
-
-  public getAutoReloadRepeat () {
-    return this.autoReloadRepeat
-  }
-
-  public interrupt () {
-    this.ghci.interrupt()
-  }
-
-  public async getCompletions (prefix) {
-    if (!prefix.trim()) {
-      return []
-    }
-    let {stdout} = await this.ghci.sendCompletionRequest()
-    stdout.shift()
-    return filter(stdout, prefix).map((text) => ({text: text.slice(1, -1)}))
   }
 
   public getURI () {
@@ -196,10 +93,8 @@ export class IdeHaskellReplView {
 
   public async destroy () {
     etch.destroy(this)
-    if (this.ghci) {
-      this.ghci.destroy()
-    }
     this.disposables.dispose()
+    super.destroy()
   }
 
   public serialize (): IViewStateOutput {
@@ -209,6 +104,20 @@ export class IdeHaskellReplView {
       content: this.messages,
       history: this.history.serialize(),
       autoReloadRepeat: this.autoReloadRepeat,
+    }
+  }
+
+  public async update () {
+    let atEnd = !!this.refs &&
+      (this.refs.output.scrollTop + this.refs.output.clientHeight >= this.refs.output.scrollHeight)
+    let focused = !!this.refs && !!document.activeElement &&
+      (this.refs.editor.element.contains(document.activeElement))
+    await etch.update(this)
+    if (atEnd) {
+      this.refs.output.scrollTop = this.refs.output.scrollHeight - this.refs.output.clientHeight
+    }
+    if (focused) {
+      this.refs.editor.element.focus()
     }
   }
 
@@ -259,7 +168,7 @@ export class IdeHaskellReplView {
 
   private renderPrompt () {
     return (
-      <div>{this.prompt || ''} &gt;</div>
+      <div>{this.prompt || ''}&gt;</div>
     )
   }
 
@@ -276,179 +185,5 @@ export class IdeHaskellReplView {
         return <pre className={cls}>{cleanText}</pre>
       }
     })
-  }
-
-  private async update () {
-    let atEnd = !!this.refs &&
-      (this.refs.output.scrollTop + this.refs.output.clientHeight >= this.refs.output.scrollHeight)
-    let focused = !!this.refs && !!document.activeElement &&
-      (this.refs.editor.element.contains(document.activeElement))
-    await etch.update(this)
-    if (atEnd) {
-      this.refs.output.scrollTop = this.refs.output.scrollHeight - this.refs.output.clientHeight
-    }
-    if (focused) {
-      this.refs.editor.element.focus()
-    }
-  }
-
-  private async initialize (upiPromise: Promise<UPI>) {
-    this.upi = await upiPromise
-    if (!this.upi) { return this.runREPL(null) }
-
-    try {
-      let builder = await this.upi.params.get('ide-haskell-cabal', 'builder')
-      this.runREPL((builder || {}).name)
-    } catch (error) {
-      if (error) {
-        atom.notifications.addFatalError(error.toString(), {
-          detail: error,
-          dismissable: true,
-        })
-        this.destroy()
-      } else {
-        atom.notifications.addWarning("Can't run REPL without knowing what builder to use")
-        this.destroy()
-      }
-    }
-  }
-
-  private async runREPL (builder: string) {
-    this.editor.element.focus()
-    this.disposables.add(atom.workspace.onDidChangeActivePaneItem((item) => {
-      if (item === this) { setImmediate(() => { this.editor.element.focus() }) }
-    }))
-
-    if (!builder) { builder = atom.config.get('ide-haskell-repl.defaultRepl') }
-    let subst = {
-      'nix-build': 'cabal',
-      'none': 'ghci',
-    }
-    builder = (subst[builder] || builder)
-
-    let [cabalFile] =
-      this.cwd.getEntriesSync().filter((file) =>
-        file.isFile() && file.getBaseName().endsWith('.cabal'))
-
-    let cabal, comp
-    if (cabalFile) {
-      let cabalContents = cabalFile.readSync()
-      cabal = Util.parseDotCabalSync(cabalContents)
-      [comp] = Util.getComponentFromFileSync(cabalContents, this.cwd.relativize(this.uri))
-    }
-    let commandPath = atom.config.get(`ide-haskell-repl.${builder}Path`)
-
-    let args = {
-      stack: ['ghci'],
-      cabal: ['repl'],
-      ghci: [],
-    }
-    let extraArgs = {
-      stack: (x) => '--ghci-options="#{x}"',
-      cabal: (x) => '--ghc-option=#{x}',
-      ghci: (x) => x,
-    }
-
-    if (!args[builder]) { throw new Error('Unknown builder #{builder}') }
-    let commandArgs = args[builder]
-
-    commandArgs.push(...(atom.config.get('ide-haskell-repl.extraArgs').map(extraArgs[builder])))
-
-    if (comp) {
-      if (builder === 'stack') {
-        if (comp.startsWith('lib:')) {
-          comp = 'lib'
-        }
-        comp = `${cabal.name}:${comp}`
-        commandArgs.push('--main-is', comp)
-      } else { commandArgs.push(comp) }
-    }
-
-    this.ghci = new GHCI({
-      atomPath: process.execPath,
-      command: commandPath,
-      args: commandArgs,
-      cwd: this.cwd.getPath(),
-      onExit: async (code) => this.destroy(),
-    })
-
-    await this.ghci.waitReady()
-
-    this.ghci.load(this.uri, (type, text) => {
-      if (type === 'prompt') {
-        this.prompt = text[1]
-        this.update()
-      }
-    })
-  }
-
-  private errorsFromStderr (stderr: string[]): boolean {
-    this.errors = this.errors.filter(({_time}) => Date.now() - _time < 10000)
-    let hasErrors = false
-    for (let err of stderr.join('\n').split(/\n(?=\S)/)) {
-      if (err) {
-        let error = this.parseMessage(err)
-        this.errors.push(error)
-        if (error.severity === 'error') {
-          hasErrors = true
-        }
-      }
-    }
-    if (this.upi) {
-      this.upi.messages.set(this.errors)
-    } else {
-      this.update()
-    }
-    return hasErrors
-  }
-
-  private unindentMessage (message): string {
-    let lines = message.split('\n').filter((x) => !x.match(/^\s*$/))
-    let minIndent = null
-    for (let line of lines) {
-      let match = line.match(/^\s*/)
-      let lineIndent = match[0].length
-      if (lineIndent < minIndent || !minIndent) { minIndent = lineIndent }
-    }
-    console.error(minIndent, lines)
-    if (minIndent) {
-      lines = lines.map((line) => line.slice(minIndent))
-    }
-    return lines.join('\n')
-  }
-
-  private parseMessage (raw): IErrorItem {
-    let matchLoc = /^(.+):(\d+):(\d+):(?: (\w+):)?\s*(\[[^\]]+\])?/
-    if (raw && raw.trim() !== '') {
-      let matched = raw.match(matchLoc)
-      if (matched) {
-        let msg = raw.split('\n').slice(1).join('\n')
-        let [file, line, col, rawTyp, context]: String[] = matched.slice(1)
-        let typ: Severity = rawTyp ? rawTyp.toLowerCase() : 'error'
-        if (file === '<interactive>') {
-          file = null
-          typ = 'repl'
-        }
-
-        // NOTE: this is done because typescript insists strings dont have
-        // trimRight() method
-        let msgany = msg as IMyString
-
-        return {
-          uri: file ? this.cwd.getFile(this.cwd.relativize(file)).getPath() : null,
-          position: [parseInt(line as string, 10) - 1, parseInt(col as string, 10) - 1],
-          message: this.unindentMessage(msgany.trimRight()),
-          context: context as string,
-          severity: typ,
-          _time: Date.now(),
-        }
-      } else {
-        return {
-          message: raw,
-          severity: 'repl',
-          _time: Date.now(),
-        }
-      }
-    }
   }
 }
